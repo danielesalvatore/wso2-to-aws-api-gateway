a795ad8de9c526796bff91e6deb2ec072f2ca3a5
Daniele Salvatore <daniele.salvatore@fao.org>
2019-10-11 16:03:50 +0200

    Remove /bin folder

--- bin/lambda.js
+++
@@ -1,5 +0,0 @@
-const serverlessExpress = require('aws-serverless-express');
-const app = require("../src/app");
-const server = serverlessExpress.createServer(app);
-
-module.exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
